59ef68212a768e9eee001e84fa0c90932e4de2a9
Iacopo <iacopo.ghila@gmail.com>
2020-11-19 16:39:32 +0100

    fatta funzione game

--- js/script.js
+++ js/script.js
@@ -6,18 +6,64 @@
 // La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
 
+
+//creo la funzione che mi generi un numero random
 function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
+
+//includes
+
+// function isIncludesInArray(arg, arr) {
+
+//     for(var k = 0; k < arr.length; k++) {
+//         if (arr[k] = arg) {
+//             return true;
+//         } else return false;
+//     }
+// }
+
+
+//genero 16 numeri random senza duplicati
+function arrGenerator(arr, num) {
+
+    for (var i = 0; i < num; i++) {
+        var random = randomNumber(1, 100);
+    
+        while (arr.includes(random)) {
+            random = randomNumber(1, 100);
+        }
+        arr.push(random);
+    }
+
+}
+
+
+//funzione che esegue il gioco
+function game(maxNum, arr) {
+
+    var count = 0;
+    var userInput = parseInt(prompt("Un numero da 1 a 100"));
+
+    while (count < maxNum && (!arr.includes(userInput))) {
+        count++;
+        console.log(count);
+        userInput = parseInt(prompt("Un numero da 1 a 100"));
+    }
+    
+    return count;
+}
+
+//--------------------------------------------------------------------------
+
 var randomArr = [];
 
-//genero 16 numeri random
-for (var i = 0; i < 16; i++) {
-    var random = randomNumber(1, 100)
+arrGenerator(randomArr, 16);
 
-    while (randomArr.indexOf(random) != -1) {
-        random = randomNumber(1, 100);
-    }
-    randomArr.push(random);
-}
+console.log(randomArr);
+
+numbers = 100 - randomArr.length;
+console.log(numbers);
+
+console.log(game(4, randomArr));
